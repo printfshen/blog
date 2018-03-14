7f7543e730d761ed7e587a25816c0b6f9c73e122
沈枫山 <564435314@qq.com>
2018-03-14 21:01:00 +0800

    提交 bootstrap

---
+++ web/js/admin/common.js
@@ -0,0 +1,12 @@
+;
+var common_ops = {
+    init:function () {
+        this.eventBind();
+    },
+    eventBind:function () {
+        var that = this;
+    }
+};
+$(document).ready(function () {
+    common_ops.init();
+});
