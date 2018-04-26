bea7cef0cd3de1fa5cee0cff75644f3aed674177
沈枫山 <564435314@qq.com>
2018-04-27 00:11:41 +0800

    Switchery.js  按钮问题解决

--- web/js/admin/account/set.js
+++ web/js/admin/account/set.js
@@ -5,13 +5,19 @@ var admin_account_set_ops = {
     },
     eventBind: function () {
         var that = this;
-        $(".admin_account_set_ops .save").click(function () {
 
+        var clickCheckbox = document.querySelector(".toggle-switch"),
+            clickButton = document.querySelector(".save");
+        clickButton.addEventListener("click", function () {
             var btn_target = $(this);
             if (btn_target.hasClass("disabled")) {
-                common_ops.alert("111");
+                common_ops.alert("正在处理!!请不要重复提交");
             }
 
+            var status = 0;
+            if (clickCheckbox.checked) {
+                status = 1;
+            }
             var nickname_target = $(".admin_account_set_ops input[name=nickname]");
             var nickname = nickname_target.val();
 
@@ -21,26 +27,34 @@ var admin_account_set_ops = {
             var email_target = $(".admin_account_set_ops input[name=email]");
             var email = email_target.val();
 
-            var nickname_target = $(".admin_account_set_ops input[name=nickname]");
-            var nickname = nickname_target.val();
-
             var login_name_target = $(".admin_account_set_ops input[name=login_name]");
             var login_name = login_name_target.val();
 
             var login_pwd_target = $(".admin_account_set_ops input[name=login_pwd]");
             var login_pwd = login_pwd_target.val();
 
-            // var status = $("input[name=status]").val();
+            if (!/^[\u4E00-\u9FA5A-Za-z0-9]{2,20}$/.test(nickname)) {
+                common_ops.tip("昵称只支持中文、字母、数字的组合，2-20个字符", nickname_target);
+                return;
+            }
+            if (!/^1[345678][0-9]{9}$/.test(mobile)) {
+                common_ops.tip("请输入符合要求的手机号码", mobile_target);
+                return;
+            }
+            if (!/^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/.test(email)) {
+                common_ops.tip("请输入符合要求的邮箱", email_target);
+                return;
+            }
+            if (!/^[a-zA-z][a-zA-Z0-9]{5,20}$/.test(login_name)) {
+                common_ops.tip("登陆名只支持字母开头和数字的组合，2-20个字符", login_name_target);
+                return;
+            }
+            if (login_pwd.length < 5 || login_pwd.length > 20) {
+                common_ops.tip("请输入5-20位的密码", login_pwd_target);
+            }
 
-            var clickCheckbox = document.querySelector(".js-check-chick"),
-            clickButton = document.querySelector(".js-check-chick-button");
 
-            clickButton.addEventListener("click", function () {
-                alert(clickCheckbox.checked)
-            })
-
-
-        })
+        });
     }
 };
 $(document).ready(function () {
